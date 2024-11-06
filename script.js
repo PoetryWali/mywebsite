7f19d99667908cf954e172cbe5607f80833d4a08
PoetryWali <akan15799@gmail.com>
2024-11-06 19:21:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    alert("Welcome to PoetryWala! Here you will find all kinds of poems and quotes, including special ones made just for you, to share your feelings. Good Luck!");
+}
